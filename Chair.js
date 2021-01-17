fc38db554b77f5ddfa21be38a940b95bb7325b14
yeeet496 <75123149+yeeet496@users.noreply.github.com>
2021-01-17 16:53:46 +0530

    Add files via upload

---
+++ Chair.js
@@ -0,0 +1,21 @@
+class chair{
+constructor(bodyA,bodyB){
+var options = {
+    bodyA :bodyA,
+    bodyB : bodyB,
+    stiffness : 0.04,
+    
+    length : 10 
+}
+this.chain = constaint.create(options)
+World.add(world,this.chain)
+}
+
+display(){
+   var pointA = this.chain.bodyA.position
+   var pointB = this.chain.bodyB.position
+   strokeWeight(4)
+   line(pointA.x, pointA.y, pointB.x, pointB.y);
+ }
+
+}
